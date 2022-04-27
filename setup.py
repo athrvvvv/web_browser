a4987a07cdaada1f34d9e98210e05b1e6d1f4cba
athrvvv <codingbruh.ac@gmail.com>
2022-04-27 20:25:45 +0530

    Setup file :)

---
+++ setup.py
@@ -0,0 +1,6 @@
+from cx_Freeze import setup, Executable
+  
+setup(name = "WEB-BROWSER" ,
+      version = "1.0.0" ,
+      description = "WEB-BROWSER-BRO" ,
+      executables = [Executable("main.py")])
