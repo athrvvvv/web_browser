3e523dbddaf23981bd1a8faf132edebe2b78c14f
athrv <codingbruh.ac@gmail.com>
2022-05-04 21:55:36 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "Web-Browser",
-    version = "1.0.0",
-    description = "Web-Browser :)",
-    executables = [Executable("main.py", base = "Win32GUI", icon="icon.ico")])
